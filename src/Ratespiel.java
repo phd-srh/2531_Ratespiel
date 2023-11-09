ca01a07c63b97b67b001eed683e2e3fc29f9e22c
Dr. Dillinger <peter.dillinger@srh.de>
2023-11-09 10:54:54 +0100

    Anzahl der Versuche wird protokolliert und bei Eingabe der 0 wird das Programm vorzeitig beendet.

--- src/Ratespiel.java
+++ src/Ratespiel.java
@@ -3,22 +3,36 @@ import java.util.Scanner;
 public class Ratespiel {
     public static void main(String[] args) {
         Scanner eingabe = new Scanner(System.in);
-        int geheime_zahl = (int)(Math.random() * 90 + 10);
+        int geheime_zahl = (int)(Math.random() * 9000 + 1000);
 
+        int anzahlVersuche = 0;
         while (true) {
             System.out.print("Wie lautet die Zahl: ");
             int zahl = eingabe.nextInt();
+            if (zahl == 0) {
+                System.out.println("Schade, viel Erfolg beim nächsten Mal");
+                System.out.println("Die gesuchte Zahl ist " + geheime_zahl);
+                break;
+            }
 
+            anzahlVersuche++;
             if (zahl == geheime_zahl) {
                 System.out.println("Jawoll, die Zahl ist richtig!");
                 break;
             }
+            else if (zahl - 100 > geheime_zahl) {
+                System.out.println("Die Zahl ist viel zu groß");
+            }
             else if (zahl > geheime_zahl ) {
                 System.out.println("Die Zahl ist zu groß");
+            }
+            else if (zahl + 100 < geheime_zahl) {
+                System.out.println("Die Zahl ist viel zu klein");
             }
             else {
                 System.out.println("Die Zahl ist zu klein");
             }
         }
+        System.out.println("Sie haben " + anzahlVersuche + " Versuche gebraucht");
     }
 }
