eef58449aa81f2e871efef3341e8fdb63910202f
Dr. Dillinger <peter.dillinger@srh.de>
2023-11-15 09:32:18 +0100

    Einführung in das Thema "Methoden"!

--- src/Ratespiel.java
+++ src/Ratespiel.java
@@ -1,9 +1,46 @@
 import java.util.Scanner;
 
 public class Ratespiel {
+
+    // Methoden - Unterprogramme, Prozeduren/Funktionen, Subroutinen
+
+    // Aufbau von Methoden
+    // Methode = Methodenkopf + Methodenrumpf
+
+    // Methodenkopf = Sichtbarkeitsattribut (public) Modifikator (static)
+    //                Datentyp Methodenname ( Parameterliste )
+    // Datentyp = void | int | boolean | double | char | ...
+    // Parameterlist = leer | Datentyp Parametername [, ...]
+    // Methodensignatur = Methodenname + Datentypen aller Parameter (Überladung)
+
+    // Feine Unterscheidung (in manchen Programmiersprachen)
+    // Methode mit Datentyp (int, boolean, ...) -> Funktion
+    // Methode ohne Datentyp (void)             -> Prozedur
+
+    // z.B.
+    public static double berechneQuadrat ( double x ) {
+        return x * x;
+    }
+
+    // z.B. Ausgabe einer Zahl
+    public static void ausgabeZahl( double zahl ) {
+        System.out.println("Das ist meine Zahl: " + zahl );
+    }
+
+
+//  /--- Methodenkopf -------------------\ /--- Methodenrumpf ---\
     public static void main(String[] args) {
+
+        //double ergebnis = ;
+        System.out.println( berechneQuadrat( 163 ) );
+
+        ausgabeZahl( 17 );
+        ausgabeZahl( 22 );
+        ausgabeZahl( 3.4 );
+
+
         Scanner eingabe = new Scanner(System.in);
-        int geheime_zahl = (int)(Math.random() * 900 + 100);
+        final int geheime_zahl = (int)(Math.random() * 900 + 100);
 
         //System.out.println("Achtung, nur zum Testen! Geheime Zahl = " + geheime_zahl);
         int anzahlVersuche = 0;
@@ -20,14 +57,14 @@ public class Ratespiel {
             // Anzahl korrekter Ziffern bestimmen (siehe Struktogramm)
             int korrekteZiffern = 0;
 
-            int gesuchteZahl = geheime_zahl;
-            while ( gesuchteZahl > 0 ) {
-                int zifferGeheimeZahl = gesuchteZahl % 10;
+            int kopieDerGeheimenZahl = geheime_zahl;
+            while ( kopieDerGeheimenZahl > 0 ) {
+                int zifferGeheimeZahl = kopieDerGeheimenZahl % 10;
                 int zifferTipp = zahl % 10;
                 if (zifferGeheimeZahl == zifferTipp) {
                     korrekteZiffern++;
                 }
-                gesuchteZahl = gesuchteZahl / 10;
+                kopieDerGeheimenZahl = kopieDerGeheimenZahl / 10;
                 zahl = zahl / 10;
             }
 
